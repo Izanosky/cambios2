03b1b974cb3ac7e51e7ce782a1667c060c31bef8
Izan Jiménez <darkop895@gmail.com>
2024-04-27 12:51:34 +0200

    asignacion memoria

--- cambios2/cambios2.cpp
+++ cambios2/cambios2.cpp
@@ -80,13 +80,6 @@ int main(int argc, char* argv[]) {
         } 
 
         //FUNCIONES SIN ARGUMENTOS
-        FARPROC refrescar = GetProcAddress(lib, "refrescar");
-        if (refrescar == NULL) {
-            printf("No se ha podido cargar la funcion\r\n");
-            fflush(stdout);
-            exit(1);
-        }
-
         FARPROC finCambios = GetProcAddress(lib, "finCambios");
         if (finCambios == NULL) {
             printf("No se ha podido cargar la funcion\r\n");
@@ -185,7 +178,12 @@ int main(int argc, char* argv[]) {
 		}
 
 		//MECANISMO DE SINCRONIZACION
-		HANDLE event = CreateEvent(NULL, FALSE, FALSE, "CreacionHijos");
+		HANDLE mutexHijos = CreateMutex(NULL, FALSE, "mtxHijos");
+		if (mutexHijos == NULL) {
+			printf("Error al crear el mutex (%d)\n", GetLastError());
+			fflush(stdout);
+			exit(1);
+		}
 
 		//CARGAMOS LA LIBRERIA
         HINSTANCE lib = LoadLibrary("cambios2.dll");
@@ -204,20 +202,6 @@ int main(int argc, char* argv[]) {
             exit(1);
         }
 
-        FARPROC incrementarCuenta = GetProcAddress(lib, "incrementarCuenta");
-        if (incrementarCuenta == NULL) {
-            printf("No se ha podido cargar la funcion\r\n");
-            fflush(stdout);
-            exit(1);
-        }
-
-        FARPROC refrescar = GetProcAddress(lib, "refrescar");
-        if (refrescar == NULL) {
-            printf("No se ha podido cargar la funcion\r\n");
-            fflush(stdout);
-            exit(1);
-        }
-
 		//FUNCIONES CON ARGUMENTOS
         inicioCambiosHijo = (INT(*) (INT, HANDLE, CHAR*)) GetProcAddress(lib, "inicioCambiosHijo");
         if (inicioCambiosHijo == NULL) {
@@ -226,18 +210,11 @@ int main(int argc, char* argv[]) {
             exit(1);
         }
 
-        aQueGrupo = (INT(*) (INT)) GetProcAddress(lib, "aQuEGrupo");
-        if (aQueGrupo == NULL) {
-            printf("No se ha podido cargar la funcion\r\n");
-            fflush(stdout);
-            exit(1);
-        }
-
-		val = inicioCambiosHijo(0, mtxLib, refM);
+		val = inicioCambiosHijo(velH, mtxLib, refM);
 
         HANDLE alumnos[32];
 
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < 32; i++) {		
 			alumnos[i] = CreateThread(NULL, 0, Alumnos, LPVOID(i), 0, NULL);
 			if (alumnos[i] == NULL) {
 				printf("Error al crear el hilo (%d)\r\n", GetLastError());
@@ -245,6 +222,11 @@ int main(int argc, char* argv[]) {
 				exit(1);
 			}
         }
+
+		WaitForMultipleObjects(32, alumnos, TRUE, INFINITE);
+		for (int i = 0; i < 32; i++) {
+			CloseHandle(alumnos[i]);
+		}
 
         FreeLibrary(lib);
 
@@ -262,22 +244,54 @@ int main(int argc, char* argv[]) {
     }
 }
 
-DWORD WINAPI Zacarias (LPVOID param) {
-
-    return 0;
-}
-
 DWORD WINAPI Alumnos (LPVOID param) {
     int posi = (int)param;
-    int aux;
-    char identificador;
+    char identificador, grupoA;
+    char vacio = ' ';
 
-    char grupo1[] = {'A', 'B', 'C', 'D', 'a', 'b', 'c', 'd'};
-    char grupo2[] = { 'E', 'F', 'G', 'H', 'e', 'f', 'g', 'h'};
-    char grupo3[] = { 'I', 'J', 'L', 'M', 'i', 'j', 'l', 'm'};
-    char grupo4[] = { 'N', 'O', 'P', 'R', 'n', 'o', 'p', 'r'};
+    char grupo[] = {'A', 'B', 'C', 'D', 'a', 'b', 'c', 'd',
+                    'E', 'F', 'G', 'H', 'e', 'f', 'g', 'h',
+                    'I', 'J', 'L', 'M', 'i', 'j', 'l', 'm',
+                    'N', 'O', 'P', 'R', 'n', 'o', 'p', 'r' };
 
-	HANDLE ev = OpenEvent(EVENT_ALL_ACCESS, FALSE, "CreacionHijos");
+	HANDLE mutexHijos = OpenMutex(MUTEX_ALL_ACCESS, FALSE, "mtxHijos");
+    if (mutexHijos == NULL) {
+		printf("Error al abrir el mutex (%d)\n", GetLastError());
+		fflush(stdout);
+		exit(1);
+    }
+
+    //CARGAMOS LA LIBRERIA
+    HINSTANCE lib = LoadLibrary("cambios2.dll");
+
+    if (lib == NULL) {
+        printf("No se pudo cargar la libreria\r\n");
+        fflush(stdout);
+        exit(1);
+    }
+
+	//FUNCIONES SIN ARGUMENTOS
+    FARPROC incrementarCuenta = GetProcAddress(lib, "incrementarCuenta");
+    if (incrementarCuenta == NULL) {
+        printf("No se ha podido cargar la funcion\r\n");
+        fflush(stdout);
+        exit(1);
+    }
+
+    FARPROC refrescar = GetProcAddress(lib, "refrescar");
+    if (refrescar == NULL) {
+        printf("No se ha podido cargar la funcion\r\n");
+        fflush(stdout);
+        exit(1);
+    }
+
+	//FUNCIONES CON ARGUMENTOS
+    aQueGrupo = (INT(*) (INT)) GetProcAddress(lib, "aQuEGrupo");
+    if (aQueGrupo == NULL) {
+        printf("No se ha podido cargar la funcion\r\n");
+        fflush(stdout);
+        exit(1);
+    }
 
     //INICIAMOS LA MEMORIA COMPARTIDA
     HANDLE mem = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "memoria");
@@ -295,45 +309,117 @@ DWORD WINAPI Alumnos (LPVOID param) {
     }
 
     //ASIGNACION INICIAL
+	WaitForSingleObject(mutexHijos, INFINITE);
     if (posi < 8) {
-		identificador = grupo1[posi];
+		identificador = grupo[posi];
+        grupoA = 1;
+
+		printf("Alumno %c en grupo %d(%d)\n", identificador, grupoA, posi);
+
+        refM = refM + posi * 2;
+		CopyMemory(refM, &identificador, sizeof(identificador));
+		refM = refM + 1;
+		CopyMemory(refM, &grupoA, sizeof(grupoA));
 
         if (posi == 7) {
-
+            for (int j = 0; j < 2; j++) {
+				refM = refM + 1;
+				CopyMemory(refM, &vacio, sizeof(vacio));
+				refM = refM + 1;
+				CopyMemory(refM, &grupoA, sizeof(grupoA));
+            }
+			refM = refM - 4;
         }
-        //INICNIAMOS EL EVENTO
 
+        refM = refM - posi * 2 - 1;
+		refrescar();		
+		ReleaseMutex(mutexHijos);
 	}
-	else if (8 <= posi < 16) {
-        aux = posi - 8;
-		identificador = grupo2[aux];
-        posi += 2;
+	else if (8 <= posi && posi < 16) {
+		identificador = grupo[posi];
+        posi = posi + 2;
+        grupoA = 2;
+
+        printf("Alumno %c en grupo %d(%d)\n", identificador, grupoA, posi);
+
+        refM = refM + posi * 2;
+        CopyMemory(refM, &identificador, sizeof(identificador));
+        refM = refM + 1;
+        CopyMemory(refM, &grupoA, sizeof(grupoA));
 
         if (posi == 17) {
-
+            for (int j = 0; j < 2; j++) {
+                refM = refM + 1;
+                CopyMemory(refM, &vacio, sizeof(vacio));
+                refM = refM + 1;
+                CopyMemory(refM, &grupoA, sizeof(grupoA));
+            }
+            refM = refM - 4;
         }
+
+        refM = refM - posi * 2 - 1;
+        refrescar();   
+        ReleaseMutex(mutexHijos);
 	}
-	else if (16 <= posi < 24) {
-		aux = posi - 16;
-		identificador = grupo3[aux];
-		posi += 4;
+	else if (16 <= posi && posi < 24) {
+		identificador = grupo[posi];
+        posi = posi + 4;
+        grupoA = 3;
 
+        printf("Alumno %c en grupo %d(%d)\n", identificador, grupoA, posi);
+
+        refM = refM + posi * 2;
+        CopyMemory(refM, &identificador, sizeof(identificador));
+        refM = refM + 1;
+        CopyMemory(refM, &grupoA, sizeof(grupoA));
 		if (posi == 27) {
+            for (int j = 0; j < 2; j++) {
+                refM = refM + 1;
+                CopyMemory(refM, &vacio, sizeof(vacio));
+                refM = refM + 1;
+                CopyMemory(refM, &grupoA, sizeof(grupoA));
+            }
+            refM = refM - 4;
+		} 
 
-		}
+        refrescar();
+        refM = refM - posi * 2 - 1;
+        ReleaseMutex(mutexHijos);
 	}
 	else {
-		aux = posi - 24;
-		identificador = grupo4[aux];
-		posi += 6;
+		identificador = grupo[posi];
+        posi = posi + 6;
+        grupoA = 4;
+
+        printf("Alumno %c en grupo %d(%d)\n", identificador, grupoA, posi);
+
+        refM = refM + posi * 2;
+        CopyMemory(refM, &identificador, sizeof(identificador));
+        refM = refM + 1;
+        CopyMemory(refM, &grupoA, sizeof(grupoA));
 
 		if (posi == 37) {
+            for (int j = 0; j < 2; j++) {
+                refM = refM + 1;
+                CopyMemory(refM, &vacio, sizeof(vacio));
+                refM = refM + 1;
+                CopyMemory(refM, &grupoA, sizeof(grupoA));
+            }
+            refM = refM - 4;
+		} 
 
-		}
-	}
+        refrescar();
+        refM = refM - posi * 2 - 1;
+        ReleaseMutex(mutexHijos);
+	} 
 
 	UnmapViewOfFile(refM);
 	CloseHandle(mem);
+
+    return 0;
+}
+
+DWORD WINAPI Zacarias(LPVOID param) {
 
     return 0;
 }
